202b79b1fc54727afef5a4dc567d438a30b0caa6
nt1983 <73156205+nt1983@users.noreply.github.com>
2020-12-06 23:32:33 -0800

    Pypoll code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,72 +3,66 @@ import os
 
 csvfilepath='PyPoll\\Resources\\election_data.csv'
 
-changes_average=0
-PL_Change_Dic={
-    "PL_Dates":[],
-    "PL_Changes":[]
+Candidates_Dic={
+    "Name":[],
+    "Vote_percent":[],
+    "Candidate_Votes":[]
 }
 with open(csvfilepath) as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
-    header = next(csvreader)
+    header=next(csvreader)
     
     #Move to 1st row after Header
     first_row=next(csvreader)
-    total_month=1
-    total_PL=int(first_row[1])
-    Current_row=int(first_row[1])
-
-    #Loop through all of rows except header row
+    Total_votes=1
+    Candidates_Dic["Name"].append(first_row[2])
+    Candidates_Dic["Candidate_Votes"].append(1)
+    
     for row in csvreader:
+        Total_votes+=1
+        #Find Candidate name and total of votes per candidate
+        if row[2] not in Candidates_Dic["Name"]:
+            Candidates_Dic["Name"].append(row[2])
+            Candidates_Dic["Candidate_Votes"].append(1)
+        else:
+            index=Candidates_Dic["Name"].index(row[2])
+            Candidates_Dic["Candidate_Votes"][index]+=1
 
-        #Calculate total number of months & total profit and loss
-        total_month+=1
-        total_PL=total_PL+int(row[1])
-
-        #Calculate PL Changes, Add Date and PL Change to Dictionary
-        PL_Change_Dic["PL_Dates"].append(row[0])
-        Changes=int(row[1])-Current_row
-        PL_Change_Dic["PL_Changes"].append(Changes)
-        Current_row=int(row[1])
-
-#Calculate average change
-changes_average=round((sum(PL_Change_Dic["PL_Changes"])/len(PL_Change_Dic["PL_Changes"])),2)
-Greatest_Increase=int(PL_Change_Dic["PL_Changes"][0])
-Greatest_Decrease=int(PL_Change_Dic["PL_Changes"][0])
-
-#Find Greatest increase and decrease
-for i in range(1,len(PL_Change_Dic["PL_Changes"])-1):
-    if int(PL_Change_Dic["PL_Changes"][i])>Greatest_Increase:
-        Greatest_Increase=int(PL_Change_Dic["PL_Changes"][i])
-        Increase_date=PL_Change_Dic["PL_Dates"][i]
-        Greatest_Increase=int(PL_Change_Dic["PL_Changes"][i])
-    elif int(PL_Change_Dic["PL_Changes"][i])<Greatest_Decrease:
-        Greatest_Decrease=PL_Change_Dic["PL_Changes"][i]
-        Decrease_date=PL_Change_Dic["PL_Dates"][i]    
+Winner=Candidates_Dic["Candidate_Votes"][0]
+Winner_name=Candidates_Dic["Name"][0]
+#Calculate percentage and find winner
+for i in range(len(Candidates_Dic["Name"])):
+    Candidates_Dic["Vote_percent"].append(round(Candidates_Dic["Candidate_Votes"][i]/Total_votes*100,3))
+    if Winner < Candidates_Dic["Candidate_Votes"][i]:
+        Winner=Candidates_Dic["Candidate_Votes"][i]
+        Winner_name=Candidates_Dic["Name"][i]
 
 #Print result on terminal
-print("Financial Analysis")
+print("Election Results")
 print("----------------------------")
-print(f"Total Month: {total_month}")
-print(f"Total: ${total_PL}")
-print(f"Average Change: ${changes_average}")
-print(f"Greatest Increase in Profits: {Increase_date} (${Greatest_Increase})")
-print(f"Greatest Decrease in Profits: {Decrease_date} (${Greatest_Decrease})")
+print(f"Total Votes: {Total_votes}")
+print("----------------------------")
+for i in range(len(Candidates_Dic["Name"])):
+    vote=Candidates_Dic["Vote_percent"][i]
+    print(str(Candidates_Dic["Name"][i])+": "+f"{vote:.3f}% ("+str(Candidates_Dic["Candidate_Votes"][i])+")")
+print("----------------------------")
+print(f"Winner: {Winner_name}")
 
 #Send result to TXT file
-txtfilepath='PyBank\\Analysis\\output.txt'
+txtfilepath='PyPoll\\Analysis\\output.txt'
 with open(txtfilepath, "w") as txt_file:
-    txt_file.write("Financial Analysis")
+    txt_file.write("Election Results")
     txt_file.write("\n")
     txt_file.write("----------------------------")
     txt_file.write("\n")
-    txt_file.write(f"Total Month: {total_month}")
+    txt_file.write(f"Total Votes: ${Total_votes}")
     txt_file.write("\n")
-    txt_file.write(f"Total: ${total_PL}")
+    txt_file.write("----------------------------")
     txt_file.write("\n")
-    txt_file.write(f"Average Change: ${changes_average}")
+    for i in range(len(Candidates_Dic["Name"])):
+        vote=Candidates_Dic["Vote_percent"][i]
+        txt_file.write(str(Candidates_Dic["Name"][i])+": "+f"{vote:.3f}% ("+str(Candidates_Dic["Candidate_Votes"][i])+")")
+        txt_file.write("\n")
+    txt_file.write("----------------------------")
     txt_file.write("\n")
-    txt_file.write(f"Greatest Increase in Profits: {Increase_date} (${Greatest_Increase})")
-    txt_file.write("\n")
-    txt_file.write(f"Greatest Decrease in Profits: {Decrease_date} (${Greatest_Decrease})")
-    
+    txt_file.write(f"Winner: {Winner_name}")
